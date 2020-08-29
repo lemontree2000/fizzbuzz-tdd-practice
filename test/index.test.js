5bd90a1a59be973daa37cbdfd73d66a88e36a211
lemontree2000 <gonelove.guo@gmail.com>
2020-08-29 23:40:45 +0800

    fizzBuzz TDD - 20200829

--- test/index.test.js
+++ test/index.test.js
@@ -1,30 +1,31 @@
 import fizzBuzz from '../index'
-describe('test FizzBuzz Game', () => {
+describe('test FuzzBuzz Game', () => {
     test('当输入数字包含3时，应该返回Fizz', () => {
-        expect(fizzBuzz(23)).toBe('Fizz')
+        expect(fizzBuzz(33)).toBe('Fizz')
     })
 
     test('当输入数字时3的倍数时，应该返回Fizz', () => {
         expect(fizzBuzz(6)).toBe('Fizz')
     })
 
-    test('当输入数字是5的倍数时，应该返回Buzz', () => {
+
+    test('当输入数字是5的倍数是， 应该返回Buzz', () => {
         expect(fizzBuzz(10)).toBe('Buzz')
     })
 
-    test('当输入数字时7的倍数时，应该返回Whizz', () => {
-        expect(fizzBuzz(7)).toBe('Whizz')
+    test('当输入数字是7的倍数时，应该返回Whizz', () => {
+        expect(fizzBuzz(14)).toBe('Whizz')
     })
 
     test('当输入数字时3和5的倍数时，应该返回FizzBuzz', () => {
         expect(fizzBuzz(15)).toBe('FizzBuzz')
     })
 
-    test('当输入数字时3和5和7的倍数时， 应该返回FizzBuzzWhizz', () => {
+    test('当输入数字时3和5和7的倍数时，应该返回FizzBuzzWhizz', () => {
         expect(fizzBuzz(105)).toBe('FizzBuzzWhizz')
     })
 
-    test('当输入数字时5和7的倍数时，应该返回BuzzWhizz', () => {
+    test('当输入数字是5和7 的倍数时，应该返回BuzzWhizz', () => {
         expect(fizzBuzz(70)).toBe('BuzzWhizz')
     })
 
