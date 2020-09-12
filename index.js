922925a4b2ab3aed35376d59db7ca77bb10d9ad4
lemontree2000 <gonelove.guo@gmail.com>
2020-09-12 23:23:20 +0800

    fizzBuzz tdd 202009012

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 export default (number) => {
     let result = ''
     if (String(number).includes(3)) {
-        result += 'Fizz'
+        return 'Fizz'
     }
+
     if (number % 3 === 0) {
         result += 'Fizz'
     }
+
     if (number % 5 === 0) {
         result += 'Buzz'
     }
+
     if (number % 7 === 0) {
         result += 'Whizz'
     }
+
     return result === '' ? number : result
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,44 +1,40 @@
 import fizzBuzz from '../index'
-describe('test fizzBuzz game', () => {
-    test('当输入包含3的数，应当返回Fizz', () => {
-        const result = fizzBuzz(13)
+
+describe('test fizzBuzz', () => {
+    test('当输入包含的3的数，返回Fizz', () => {
+        const result = fizzBuzz(103)
         expect(result).toBe('Fizz')
     })
-
-    test('当输入的是3的倍数，应当返回Fizz', () => {
+    test('当输入是3的倍数，返回Fizz', () => {
         const result = fizzBuzz(6)
         expect(result).toBe('Fizz')
     })
-
-    test('当输入的是5的倍数，应当返回Buzz', () => {
+    test('当输入是5的倍数，返回Buzz', () => {
         const result = fizzBuzz(10)
         expect(result).toBe('Buzz')
     })
-
-    test('当输入的是7的倍数，应当返回Whizz', () => {
+    test('当输入是7的倍数，返回Whizz', () => {
         const result = fizzBuzz(14)
         expect(result).toBe('Whizz')
     })
-
-    test('当输入的是3和5的倍数，应当返回FizzBuzz', () => {
+    test('当输入是3和5的倍数，返回FizzBuzz', () => {
         const result = fizzBuzz(15)
         expect(result).toBe('FizzBuzz')
     })
-    test('当输入的是3和7的倍数，应当返回FizzWhizz', () => {
+    test('当输入是3和7的倍数，返回FizzWhizz', () => {
         const result = fizzBuzz(21)
         expect(result).toBe('FizzWhizz')
     })
-    test('当输入的是5和7的倍数，应当返回BuzzWhizz', () => {
+    test('当输入是5和7的倍数，返回BuzzWhizz', () => {
         const result = fizzBuzz(70)
         expect(result).toBe('BuzzWhizz')
     })
-    test('当输入的是3和5和7的倍数，应当返回FizzBuzzWhizz', () => {
+    test('当输入是3和5和7的倍数，返回FizzBuzzWhizz', () => {
         const result = fizzBuzz(105)
         expect(result).toBe('FizzBuzzWhizz')
     })
-    test('当输入的不是3和5和7的倍数，应当返回自身', () => {
+    test('当输入不是3和5和7的倍数，返回自身', () => {
         const result = fizzBuzz(4)
         expect(result).toBe(4)
     })
-
 })
