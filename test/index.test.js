5fb00b7b6b56e0b7bd28edc647e5065b5214109f
lemontree2000 <gonelove.guo@gmail.com>
2020-09-20 23:59:16 +0800

    tdd-fizzBuzz 202009020

--- test/index.test.js
+++ test/index.test.js
@@ -1,49 +1,10 @@
 import fizzBuzz from '../index'
 
 describe('test fizzBuzz game', () => {
-   test('当输入包含3 的数，返回Fizz', () => {
-       const result = fizzBuzz(13)
-       expect(result).toBe('Fizz') 
-   })
-
-   test('当输入包含3 的倍数，返回Fizz', () => {
-       const result = fizzBuzz(6)
-       expect(result).toBe('Fizz') 
-   })
-
-   test('当输入包含5 的倍数，返回Buzz', () => {
-       const result = fizzBuzz(10)
-       expect(result).toBe('Buzz') 
-   })
-
-   test('当输入包含7 的倍数，返回Whizz', () => {
-       const result = fizzBuzz(14)
-       expect(result).toBe('Whizz') 
-   })
-
-   test('当输入包含3和5 的倍数，返回FizzBuzz', () => {
-       const result = fizzBuzz(15)
-       expect(result).toBe('FizzBuzz') 
-   })
-
-   test('当输入包含3和7 的倍数，返回FizzWhizz', () => {
-       const result = fizzBuzz(21)
-       expect(result).toBe('FizzWhizz') 
-   })
-
-   test('当输入包含5和7 的倍数，返回BuzzWhizz', () => {
-       const result = fizzBuzz(70)
-       expect(result).toBe('BuzzWhizz') 
-   })
-
-   test('当输入包含3和5和7 的倍数，返回FizzBuzzWhizz', () => {
-       const result = fizzBuzz(105)
-       expect(result).toBe('FizzBuzzWhizz') 
-   })
-
-   test('当输入不包含3和5和7 的倍数，返回自身', () => {
-       const result = fizzBuzz(4)
-       expect(result).toBe(4) 
-   })
+    test('当遇到包含3的数，报Fizz', () => {
+        const result = fizzBuzz(13)
+        expect(result).toBe('Fizz')
+    })
+    
     
 })
