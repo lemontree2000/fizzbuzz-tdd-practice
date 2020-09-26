ad5ab4751972d8cdaf3a3ce43ace03a39861b936
lemontree2000 <gonelove.guo@gmail.com>
2020-09-26 23:59:11 +0800

    tdd-fizzBuzz 202009026

--- test/index.test.js
+++ test/index.test.js
@@ -1,47 +1,48 @@
 import fizzBuzz from '../index'
-
 describe('test fizzBuzz game', () => {
-    test('当输入包含3的数，报Fizz', () => {
-        const result = fizzBuzz(13);
-        expect(result).toBe('Fizz')
-    })
-    test('当输入包含3的倍数，报Fizz', () => {
-        const result = fizzBuzz(6);
+    test('当输入的是包含3的数，报Fizz', () => {
+        const result = fizzBuzz(13)
         expect(result).toBe('Fizz')
     })
 
-    test('当输入包含5的倍数，报Buzz', () => {
-        const result = fizzBuzz(10);
+    test('当输入的是包含3的倍数，报Fizz', () => {
+        const result = fizzBuzz(6)
+        expect(result).toBe('Fizz')
+    })
+
+    test('当输入的是包含5的倍数，报Buzz', () => {
+        const result = fizzBuzz(10)
         expect(result).toBe('Buzz')
     })
 
-    test('当输入包含7的倍数，报Whizz', () => {
-        const result = fizzBuzz(14);
+    test('当输入的是包含7的倍数，报Whizz', () => {
+        const result = fizzBuzz(14)
         expect(result).toBe('Whizz')
     })
 
-    test('当输入包含3和5的倍数，报FizzBuzz', () => {
-        const result = fizzBuzz(15);
+    test('当输入的是包含3和5的倍数，报FizzBuzz', () => {
+        const result = fizzBuzz(15)
         expect(result).toBe('FizzBuzz')
     })
 
-    test('当输入包含3和7的倍数，报FizzWhizz', () => {
-        const result = fizzBuzz(21);
+    test('当输入的是包含3和7的倍数，报FizzWhizz', () => {
+        const result = fizzBuzz(21)
         expect(result).toBe('FizzWhizz')
     })
 
-    test('当输入包含5和7的倍数，报BuzzWhizz', () => {
-        const result = fizzBuzz(70);
+    test('当输入的是包含5和7的倍数，报BuzzWhizz', () => {
+        const result = fizzBuzz(70)
         expect(result).toBe('BuzzWhizz')
     })
 
-    test('当输入包含3和5和7的倍数，报FizzBuzzWhizz', () => {
-        const result = fizzBuzz(105);
+    test('当输入的是包含3和5和7的倍数，报FizzBuzzWhizz', () => {
+        const result = fizzBuzz(105)
         expect(result).toBe('FizzBuzzWhizz')
     })
 
-    test('当输入不包含3和5和7的倍数，报输入的数', () => {
-        const result = fizzBuzz(4);
+    test('当输入的是不包含3和5和7的倍数，报自身', () => {
+        const result = fizzBuzz(4)
         expect(result).toBe(4)
     })
+
 })
